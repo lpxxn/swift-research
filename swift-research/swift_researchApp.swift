c12d4588f443e49bdb1a6101698c3e4ca838ac4f
lpxxn <mi_duo@live.com>
2023-08-18 15:38:34 +0800

    Initial Commit

---
+++ swift-research/swift_researchApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_researchApp.swift
+//  swift-research
+//
+//  Created by li on 2023/8/18.
+//
+
+import SwiftUI
+
+@main
+struct swift_researchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
